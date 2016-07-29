c0d00b38b627e0c099d0c656ac53d95ead3ae677
Billy Saysavath <bisaysavath@gmail.com>
2016-07-29 11:13:51 -0700

    Add control-minecraft

---
+++ mycroft/skills/control_minecraft/__init__.py
@@ -0,0 +1,30 @@
+import time
+from pykeyboard import PyKeyboard
+from os.path import dirname
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+
+__author__ = 'bsays'
+
+class ControlMinecraftSkill(MycroftSkill):
+    
+    def __init__(self):
+        super(ControlMinecraftSkill, self).__init__(name="ControlMinecraftSkill")
+
+    def initialize(self):
+        self.load_data_files(dirname(__file__))
+
+        move_minecraft_intent = IntentBuilder("MoveMinecraftIntent").require("MoveKeyword").build()
+        self.register_intent(move_minecraft_intent, self.handle_move_minecraft_intent)
+
+    def handle_move_minecraft_intent(self, message):
+        self.speak_dialog("move.feedback")
+        k = PyKeyboard()        
+        k.tap_key('R')
+
+    def stop(self):
+        pass
+
+def create_skill():
+    return ControlMinecraftSkill()
